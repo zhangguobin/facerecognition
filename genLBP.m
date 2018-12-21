77c3fc5eb7005f470f5994aa2a97d5baa0dd50cc
Guobin Zhang <guobin.zhang@intel.com>
2018-12-21 10:52:25 +0800

    Fix another bug in LBP implementation for local block
    Understand block vs neighborhood concepts in LBP, e.g. 16x16 cell,
    3x3 (8,1) neighborhood.
    
    Pattern is represented by clockwise or counter-clockwise sequence,
    not the sequence generated by im2col.
    
    Replace the loop with arrayfun
    
    Signed-off-by: Guobin Zhang <guobin.zhang@intel.com>

--- genLBP.m
+++ genLBP.m
@@ -1,26 +1,27 @@
 function [uniformLbp] = genLBP(image)
 %genLBP Extract local binary pattern (LBP) features
-%   Detailed explanation goes here
+%    (8,1) neighborhood, uniform extension
 
-cells16by16 = im2col(image, [16,16], 'distinct');
-[~, cols] = size(cells16by16);
-lbpFeatures = zeros(1, 256);
-for i = 1:cols
-    cellMatrix = reshape(cells16by16(:,i), [16,16]);
-    cells3by3 = im2col(cellMatrix, [3,3], 'sliding');
-    [~, count] = size(cells3by3);
-    tempCells = cells3by3;
-    tempCells(5, :) = [];
-    results = tempCells >= cells3by3(5, :);
-    values = zeros(count);
-    bitmasks = num2str(transpose(results));
-    for j = 1:count
-        temp = bitmasks(j, :);
-        temp(isspace(temp)) = '';
-        values(j) = bin2dec(temp);
-    end
-    lbpFeatures = lbpFeatures + histcounts(values, 0:256);
-end
+cells3by3 = im2col(image, [3,3], 'sliding');
+tempCells = cells3by3;
+tempCells(5, :) = [];
+results = tempCells >= cells3by3(5, :);
+% permute rows by couter-clockwise from top-left
+idx = [1 2 3 5 8 7 6 4];
+% clockwise from the center right
+% idx = [7 6 4 1 2 3 5 8];
+patterns = num2str(transpose(results(idx, :)));
+values = arrayfun(@(pattern) bin2dec(strrep(pattern, ' ', '')),...
+    string(patterns));
+% [~, count] = size(cells3by3);
+% values = zeros(1, count);
+% for j = 1:count
+%     temp = patterns(j, :);
+%     temp(isspace(temp)) = '';
+%     values(j) = bin2dec(temp);
+% end
+lbpFeatures = histcounts(values, 0:256);
+
 % uniform LBP
 % TODO get rid of hard coding
 uniPatterns = [0, 1, 2, 3, 4, 6, 7, 8, 12, 14, 15, 16, 24, 28,...
@@ -28,8 +29,9 @@ uniPatterns = [0, 1, 2, 3, 4, 6, 7, 8, 12, 14, 15, 16, 24, 28,...
     128, 129, 131, 135, 143, 159, 191, 192, 193, 195, 199, 207, 223,...
     224, 225, 227, 231, 239, 240, 241, 243, 247, 248, 249, 251, 252,...
     253, 254, 255];
+otherPatterns = setdiff(0:255, uniPatterns);
 uniformLbp = zeros(1, 59);
-uniformLbp(1:58) = lbpFeatures(uniPatterns+1);
-uniformLbp(59) = sum(lbpFeatures) - sum(uniformLbp(1:58));
+uniformLbp(1:58) = lbpFeatures(uniPatterns + 1);
+uniformLbp(59) = sum(lbpFeatures(otherPatterns + 1));
 end
 
